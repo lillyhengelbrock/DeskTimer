bc2ba63ea16fb4ed568723508fe295f7c8a54d54
lillyhengelbrock <142220672+lillyhengelbrock@users.noreply.github.com>
2023-11-19 18:15:21 +0100

    add and fix all essential timer functionalities

--- desktimer.py
+++ desktimer.py
@@ -1,103 +1,133 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import asyncio
-import pygame
 from dotenv import load_dotenv
 import os
 
+# Load environment variables from .env
 load_dotenv()
 
+# Retrieve the bot token from the environment variables
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-SOUND_FILE_PATH = 'timersound.mp3'
+# Set your default work and break times in seconds
+WORK_TIME = 25 * 60  # 25 minutes
+BREAK_TIME = 5 * 60  # 5 minutes
+DEFAULT_ROUNDS = 4 # timer repeats 4 times
 
+# Create a dictionary to store voice channel IDs per server
+voice_channel_ids = {}
+timer_running=False
+# Create intents and enable the necessary ones
 intents = discord.Intents.default()
-intents.messages = True
+intents.messages = True  # Enable message content intent
 intents.message_content = True
-intents.bans = True
-intents.dm_messages = True
-intents.dm_reactions = True
-intents.dm_typing = True
-intents.emojis = True
-intents.emojis_and_stickers = True
-intents.guild_messages = True
-intents.guild_reactions = True
-intents.guild_scheduled_events = True
-intents.guild_typing = True
-intents.guilds = True
-intents.integrations = True
-intents.invites = True
-intents.messages = True # `message_content` is required to get the content of the messages
-intents.reactions = True
-intents.typing = True
-intents.voice_states = True
-intents.webhooks = True
+intents.guilds = True    # Enable guilds intent
+intents.voice_states = True  # Enable voice states intent
+
+# Create a bot instance with a command prefix and intents
 bot = commands.Bot(command_prefix='!', intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name} ({bot.user.id})')
-
-@bot.command(name='ping')
-async def ping(ctx):
-    await ctx.send('Pong!')
-
-
-WORK_TIME = 1 * 60  # 25 minutes
-BREAK_TIME = 1 * 60  # 5 minutes
-
-current_timer = None
 
 def format_time(seconds):
     minutes, seconds = divmod(seconds, 60)
     return f'{minutes:02}:{seconds:02}'
 
-def play_sound():
-    pygame.mixer.init()
-    pygame.mixer.music.load(SOUND_FILE_PATH)
-    pygame.mixer.music.play()
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+    await bot.change_presence(activity=discord.Game(name="!startpomodoro"))
 
-# Timer task
-async def timer_task(ctx, duration, message):
-    global current_timer
+# Function to play the sound in a voice channel
+async def play_sound_vc(channel):
+    if channel.guild.voice_client is None:
+        vc = await channel.connect()
+    else:
+        vc = channel.guild.voice_client
+    source = discord.FFmpegPCMAudio('timersound.mp3')
+    vc.play(source)
 
-    await ctx.send(f'Timer: {format_time(duration)}')
 
-    for remaining_time in range(duration, 0, -1):
-        await asyncio.sleep(1)
-        await message.edit(content=f'Timer: {format_time(remaining_time)}')
-
-    play_sound()
-    await ctx.send('Time is up!')
-
-    current_timer = None  # Reset the current timer
-
-# Command to start the Pomodoro timer
-@bot.command(name='startpomodoro')
-async def start_pomodoro(ctx, work_time=WORK_TIME, break_time=BREAK_TIME):
-    global current_timer
-
-    if current_timer is not None:
-        await ctx.send('A timer is already running!')
+# Timer task with dynamic message update
+async def timer_work_vc(ctx, duration):
+    global timer_running
+    channel_id = voice_channel_ids.get(ctx.guild.id)
+    if not timer_running:
         return
+    if channel_id:
+        channel = ctx.guild.get_channel(channel_id)
+        if channel:
+            message = await ctx.send(f'Work: {format_time(duration)}')
 
-    await ctx.send(f'Starting Pomodoro timer with {format_time(work_time)} work and {format_time(break_time)} break.')
+            for remaining_time in range(duration, 0, -1):
+                if not timer_running:
+                    await ctx.send('Timer interrupted.')
+                    return
+                await asyncio.sleep(1)
+                await message.edit(content=f'Work: {format_time(remaining_time)}')
 
-    # Start the work timer
-    message = await ctx.send(f'Timer: {format_time(work_time)}')
-    current_timer = bot.loop.create_task(timer_task(ctx, work_time, message))
+            await play_sound_vc(channel)
+        else:
+            await ctx.send('The voice channel no longer exists.')
+    else:
+        await ctx.send('The voice channel ID is not set. Use !setvoicechannel command to set it.')
 
-    # Wait for the work timer to finish
-    await current_timer
+async def timer_break_vc(ctx, duration):
+    global timer_running
+    channel_id = voice_channel_ids.get(ctx.guild.id)
+    if not timer_running:
+        return
+    if channel_id:
+        channel = ctx.guild.get_channel(channel_id)
+        if channel:
+            message = await ctx.send(f'Break: {format_time(duration)}')
 
-    # Start the break timer
-    await asyncio.sleep(1)  # Give a small delay between work and break
-    message = await ctx.send(f'Timer: {format_time(break_time)}')
-    current_timer = bot.loop.create_task(timer_task(ctx, break_time, message))
+            for remaining_time in range(duration, 0, -1):
+                if not timer_running:
+                    return
+                await asyncio.sleep(1)
+                await message.edit(content=f'Break: {format_time(remaining_time)}')
 
+            await play_sound_vc(channel)
+        else:
+            await ctx.send('The voice channel no longer exists.')
+    else:
+        await ctx.send('The voice channel ID is not set. Use !setvoicechannel command to set it.')
+
+# Command to set the voice channel ID for the server
+@bot.command(name='setvoicechannel')
+async def set_voice_channel(ctx, channel_id: int):
+    voice_channel_ids[ctx.guild.id] = channel_id
+    await ctx.send(f'Voice channel ID set to {channel_id} for this server.')
+
+# Command to start the Pomodoro timer with sound in the configured voice channel
+@bot.command(name='timer')
+async def start_pomodoro_vc(ctx, rounds: int =DEFAULT_ROUNDS, work_time: int =WORK_TIME, break_time: int =BREAK_TIME):
+    global timer_running
+    timer_running = True
+    await ctx.send("Timer started - Let's go!")
+    for _ in range(rounds):
+        await timer_work_vc(ctx, work_time)
+        await timer_break_vc(ctx, break_time)
+    if timer_running:
+        await ctx.send('Good job! You finished your pomodoro session!')
+    timer_running = False
+    await asyncio.sleep(5)
+    if ctx.guild.voice_client is not None:
+        await ctx.guild.voice_client.disconnect()
+
+# Command to stop the timer and disconnect from the voice channel
 @bot.command(name='stop')
-async def stop(ctx):
-    await ctx.send("Pomodoro timer stopped! ⏹️")
-    bot.loop.stop()
+async def stop_timer(ctx):
+    global timer_running
 
+    if timer_running:
+        timer_running = False
+        if ctx.guild.voice_client is not None:
+            await ctx.guild.voice_client.disconnect()
+        else:
+            await ctx.send('No voice channel to disconnect from.')
+    else:
+        await ctx.send('No timer is currently running.')
+
+# Run the bot with your token
 bot.run(BOT_TOKEN)
